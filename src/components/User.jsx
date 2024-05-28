e0250c87b35081966b2707fd64a3fe5397e65893
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-05-28 23:22:19 +0200

    fake authentication

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,8 @@
 import styles from "./User.module.css";
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Ja",
+  email: "ja@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
